096e9149d5a0e5f04d8991224fae2e3e375d7247
Satishkumar Purswani <42316009+satishpurswani@users.noreply.github.com>
2020-06-23 00:48:43 +0530

    Enabled Eureka Client

--- src/main/java/com/poc/dbserver/DbServerApplication.java
+++ src/main/java/com/poc/dbserver/DbServerApplication.java
@@ -2,7 +2,8 @@ package com.poc.dbserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class DbServerApplication {
 
